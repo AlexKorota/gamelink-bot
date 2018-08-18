aa3c739629e596c4e3b5a753b7603a917fa46f73
alex <norther48@list.ru>
2018-08-18 12:37:22 +0300

    Added regexp adn handler for created_at param

--- service/parseService.go
+++ service/parseService.go
@@ -10,13 +10,25 @@ import (
 func ParseRequest(params []string) ([]*prot.OneCriteriaStruct, error) {
 	//var criteria prot.OneCriteriaStruct
 	var multiCriteria []*prot.OneCriteriaStruct
-	ageRegexp, err := regexp.Compile("(((age)\\s*(=\\s*([0-9]{1,2})|\\[\\s*([0-9]{1,2})\\s*;\\s*([0-9]{1,2})\\s*\\])))")
+	ageRegexp, err := regexp.Compile("(((age)\\s*(=\\s*([0-9]{1,2}$)|\\[\\s*([0-9]{1,2})\\s*;\\s*([0-9]{1,2})\\s*\\]$)))")
 	if err != nil {
 		return nil, errors.New("age regexp error")
 	}
-	idRegexp, err := regexp.Compile("(((id|vk_id|fb_id)\\s*(=\\s*([0-9]{1,20})|\\[\\s*([0-9]{1,20})\\s*;\\s*([0-9]{1,20})\\s*\\])))")
+	idRegexp, err := regexp.Compile("(((id|vk_id|fb_id)\\s*(=\\s*([0-9]{1,20}$)|\\[\\s*([0-9]{1,20})\\s*;\\s*([0-9]{1,20})\\s*\\]$)))")
 	if err != nil {
-		return nil, errors.New("age regexp error")
+		return nil, errors.New("id regexp error")
+	}
+	sexRegexp, err := regexp.Compile("(((sex)\\s*(=\\s*(f|m)$)))")
+	if err != nil {
+		return nil, errors.New("sex regexp error")
+	}
+	delRegexp, err := regexp.Compile("(((deleted)\\s*(=\\s*(0|1)$)))")
+	if err != nil {
+		return nil, errors.New("delete regexp error")
+	}
+	registrationRegexp, err := regexp.Compile("(((created_at)\\s*(=\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4}$)|\\[\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4})\\s*;\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4})\\]$)))") //(((created_at)\s*(=\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4}$)|\[\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4})\s*;\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4})\]$)))
+	if err != nil {
+		return nil, errors.New("delete regexp error")
 	}
 	for _, v := range params {
 		var result []string
@@ -30,6 +42,26 @@ func ParseRequest(params []string) ([]*prot.OneCriteriaStruct, error) {
 			appendToMultiCriteria(&multiCriteria, result)
 			continue
 		}
+		result = sexRegexp.FindStringSubmatch(v)
+		if result != nil {
+			appendToMultiCriteria(&multiCriteria, result)
+			continue
+		}
+		result = delRegexp.FindStringSubmatch(v)
+		if result != nil {
+			appendToMultiCriteria(&multiCriteria, result)
+			continue
+		}
+		result = registrationRegexp.FindStringSubmatch(v)
+		if result != nil {
+			if result[8] != "" && result[11] != "" {
+				result[6] = result[8]
+				result[7] = result[11]
+			}
+			appendToMultiCriteria(&multiCriteria, result)
+			continue
+		}
+		return nil, errors.New(fmt.Sprintf("wrong param %s", v))
 	}
 	fmt.Println(multiCriteria)
 	return multiCriteria, nil
