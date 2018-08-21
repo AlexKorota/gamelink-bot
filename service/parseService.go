3dc5b4090dbd3f7bfe833391efbc3386c48a4aa1
alex <norther48@list.ru>
2018-08-21 16:42:40 +0300

    moved regexp compile to init()

--- service/parseService.go
+++ service/parseService.go
@@ -4,32 +4,38 @@ import (
 	"errors"
 	"fmt"
 	"gamelinkBot/prot"
+	"log"
 	"regexp"
 )
 
+var ageRegexp, idRegexp, sexRegexp, delRegexp, registrationRegexp *regexp.Regexp
+var err error
+
+func init() {
+	ageRegexp, err = regexp.Compile("(((age)\\s*(=\\s*([0-9]{1,2}$)|\\[\\s*([0-9]{1,2})\\s*;\\s*([0-9]{1,2})\\s*\\]$)))")
+	if err != nil {
+		log.Fatal(err)
+	}
+	idRegexp, err = regexp.Compile("(((id|vk_id|fb_id)\\s*(=\\s*([0-9]{1,20}$)|\\[\\s*([0-9]{1,20})\\s*;\\s*([0-9]{1,20})\\s*\\]$)))")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sexRegexp, err = regexp.Compile("(((sex)\\s*(=\\s*(f|m)$)))")
+	if err != nil {
+		log.Fatal(err)
+	}
+	delRegexp, err = regexp.Compile("(((deleted)\\s*(=\\s*(0|1)$)))")
+	if err != nil {
+		log.Fatal(err)
+	}
+	registrationRegexp, err = regexp.Compile("(((created_at)\\s*(=\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4}$)|\\[\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4})\\s*;\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4})\\]$)))") //(((created_at)\s*(=\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4}$)|\[\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4})\s*;\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4})\]$)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func ParseRequest(params []string) ([]*prot.OneCriteriaStruct, error) {
-	//var criteria prot.OneCriteriaStruct
 	var multiCriteria []*prot.OneCriteriaStruct
-	ageRegexp, err := regexp.Compile("(((age)\\s*(=\\s*([0-9]{1,2}$)|\\[\\s*([0-9]{1,2})\\s*;\\s*([0-9]{1,2})\\s*\\]$)))")
-	if err != nil {
-		return nil, errors.New("age regexp error")
-	}
-	idRegexp, err := regexp.Compile("(((id|vk_id|fb_id)\\s*(=\\s*([0-9]{1,20}$)|\\[\\s*([0-9]{1,20})\\s*;\\s*([0-9]{1,20})\\s*\\]$)))")
-	if err != nil {
-		return nil, errors.New("id regexp error")
-	}
-	sexRegexp, err := regexp.Compile("(((sex)\\s*(=\\s*(f|m)$)))")
-	if err != nil {
-		return nil, errors.New("sex regexp error")
-	}
-	delRegexp, err := regexp.Compile("(((deleted)\\s*(=\\s*(0|1)$)))")
-	if err != nil {
-		return nil, errors.New("delete regexp error")
-	}
-	registrationRegexp, err := regexp.Compile("(((created_at)\\s*(=\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4}$)|\\[\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4})\\s*;\\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\\.(0[1-9]|1[012])\\.[0-9]{4})\\]$)))") //(((created_at)\s*(=\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4}$)|\[\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4})\s*;\s*((0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4})\]$)))
-	if err != nil {
-		return nil, errors.New("delete regexp error")
-	}
 	for _, v := range params {
 		var matches []string
 		matches = ageRegexp.FindStringSubmatch(v)
