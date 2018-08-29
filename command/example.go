62c04c1f23fff1e5b50e9b797f71a9a9d4aaaf40
Sergey Zinchenko <z@smedialink.com>
2018-08-29 18:18:20 +0300

    bot closing

--- command/example.go
+++ command/example.go
@@ -11,14 +11,23 @@ func Example() {
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	ctx, _ := context.WithCancel(context.Background())
-	requests, _ := reactor.RequesterResponderWithContext(ctx)
+	ctx, done := context.WithCancel(context.Background())
+	requests, err := reactor.RequesterResponderWithContext(ctx)
+	if err != nil {
+		logrus.Fatal(err)
+	}
 	for req := range requests {
+		logrus.Info(req.Request())
 		//cmd, err := NewCommand(req)
 		//if err != nil {
 		//	req.Respond("Ху нью!")
 		//}
 		//cmd.ExecuteWithContext(ctx)
-		req.Respond("xyz")
+		if req.Request() == "/done" {
+			done()
+		} else {
+			req.Respond("sfidbigbigbs")
+		}
 	}
+	logrus.Info("exiting...")
 }
