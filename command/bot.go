bd8e3bd6e3eb150d41505dd23cb4baf89d1ead20
alex <norther48@list.ru>
2018-09-05 17:41:19 +0300

    added comments for funcs, interfaces and structs

--- command/bot.go
+++ command/bot.go
@@ -7,34 +7,35 @@ import (
 )
 
 type (
+	//Requester - interface for getting request info
 	Requester interface {
 		Request() string
 		UserName() string
 	}
-
+	//Responder - interface for respond
 	Responder interface {
 		Respond(message string) error
 	}
-
+	//Response - interface for getting response info
 	Response interface {
 		Response() string
 		ChatId() int64
 	}
-
+	//RequesterResponder - interface for RequesterResponder
 	RequesterResponder interface {
 		Requester
 		Responder
 	}
-
+	//Reactor - interface for react to request
 	Reactor interface {
 		RequesterResponderWithContext(ctx context.Context) (<-chan RequesterResponder, error)
 		Respond(r Response) error
 	}
-
+	//Bot - struct that contains bot
 	Bot struct {
 		bot *tgbotapi.BotAPI
 	}
-
+	//RoundTrip - struct for round trip params
 	RoundTrip struct {
 		r                           Reactor
 		chatId                      int64
@@ -42,6 +43,7 @@ type (
 	}
 )
 
+//NewBot - create new Reactor
 func NewBot(token string) (Reactor, error) {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
@@ -50,6 +52,7 @@ func NewBot(token string) (Reactor, error) {
 	return &Bot{bot}, nil
 }
 
+//RequesterResponderWithContext - listen for updates from bot, then create RoundTrip and path it to channel
 func (b Bot) RequesterResponderWithContext(ctx context.Context) (<-chan RequesterResponder, error) {
 	if ctx.Err() != nil {
 		return nil, ctx.Err()
@@ -83,6 +86,7 @@ func (b Bot) RequesterResponderWithContext(ctx context.Context) (<-chan Requeste
 	return rrchan, nil
 }
 
+//Respond - send msg to bot
 func (b Bot) Respond(r Response) error {
 	if r.Response() == "" {
 		return nil
@@ -92,18 +96,22 @@ func (b Bot) Respond(r Response) error {
 
 }
 
+//Request - return request string
 func (rt RoundTrip) Request() string {
 	return rt.request
 }
 
+//UserName - return user name who send msg to bot
 func (rt RoundTrip) UserName() string {
 	return rt.userName
 }
 
+//ChatId - return chat id
 func (rt RoundTrip) ChatId() int64 {
 	return rt.chatId
 }
 
+//Response - return response string
 func (rt RoundTrip) Response() string {
 	return rt.response
 }
