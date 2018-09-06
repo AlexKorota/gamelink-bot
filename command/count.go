1b952bf172ae393eb5e6730dad6a53708d548f0e
alex <norther48@list.ru>
2018-09-06 10:04:26 +0300

    added comments for count funcs, interfaces and structs

--- command/count.go
+++ command/count.go
@@ -7,7 +7,9 @@ import (
 )
 
 type (
-	CountFabric  struct{}
+	//CountFabic - struct for Count fabric
+	CountFabric struct{}
+	//CountCommand - struct for count command
 	CountCommand struct {
 		params []*prot.OneCriteriaStruct
 		res    Responder
@@ -15,21 +17,26 @@ type (
 )
 
 const (
+	//commandCount - const for command
 	commandCount = "count"
 )
 
+//init - func for register fabric in parser
 func init() {
 	SharedParser().RegisterFabric(CountFabric{})
 }
 
+//RequireAdmin - func for checking if admin permissions required
 func (c CountFabric) RequireAdmin() bool {
 	return false
 }
 
+//Require - return array of needed permissions
 func (c CountFabric) Require() []string {
 	return []string{commandCount}
 }
 
+//TryParse - func for parsing request
 func (c CountFabric) TryParse(req RequesterResponder) (Command, error) {
 	var (
 		command CountCommand
@@ -42,6 +49,7 @@ func (c CountFabric) TryParse(req RequesterResponder) (Command, error) {
 	return command, nil
 }
 
+//Execute - execute command
 func (cc CountCommand) Execute(ctx context.Context) {
 	r, err := SharedClient().Count(ctx, &prot.MultiCriteriaRequest{Params: cc.params})
 	if err != nil {
